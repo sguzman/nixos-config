b390a867e3c8bc5ce363c6a3e385c5b54eceb09c
Salvador Guzman <guzmansalv@gmail.com>
2020-06-25 03:05:17 -0700

    ok so it keeps moving around - dont know why but will hardcode for now :(((

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,7 @@
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/disk/by-id/ata-Samsung_SSD_860_PRO_256GB_S45GNF0K219130E"; # or "nodev" for efi only
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
